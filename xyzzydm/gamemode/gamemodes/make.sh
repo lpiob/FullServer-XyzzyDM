c11152ccdb4f09a084854092828e95497cce1130
XJL <lukasz.biegaj@gmail.com>
2012-02-21 15:33:01 +0100

    Wersja dla klientow NetShoot

--- xyzzydm/gamemode/gamemodes/make.sh
+++ xyzzydm/gamemode/gamemodes/make.sh
@@ -2,7 +2,7 @@
 
 date
 #SVER=`svnversion ../..`
-echo \#define GMVERSION \"3.3-SP\" > ../include/fullserver/version.inc
+echo \#define GMVERSION \"3.3-NetShoot\" > ../include/fullserver/version.inc
 KIEDY=`date +%x\ %T`
 GDZIE=`hostname`
 GMHOST="91.204.162.80"
@@ -13,6 +13,4 @@ echo \#define GMHOST \"$GMHOST\" >> ../include/fullserver/version.inc
 echo \#define GMPORT $GMPORT >> ../include/fullserver/version.inc
 
 WINEPREFIX=/vol/n/.wg/ wine /pawno/pawncc -i/pawno/include_0.3d -i..\\include xyzzydm.pwn  -\;\+ -\\ -\(\+
-# && ncftpput -u login -p haslo -C 91.204.162.80 fs.amx gamemodes/fs.amx
-#WINEPREFIX=/vol/n/.wg/ wine /pawno/pawncc -i/pawno/include_0.3c -i..\\include fs.pwn  -\;\+ -\\ -\(\+ && scp -P 57022 fs.amx pio@fsadm.i64.pl:~/fs/samp03c/gamemodes/
 
